f161a59a46d21ed0e10269aa7fbd679f47c66f64
leeeunmii <dms970515@gmail.com>
2018-07-24 17:04:09 -0700

    Tania_commit

---
+++ app/src/main/java/com/example/jaeheekim/sign_up/activity_find_password.java
@@ -0,0 +1,13 @@
+package com.example.jaeheekim.sign_up;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_find_password extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_password);
+    }
+}
